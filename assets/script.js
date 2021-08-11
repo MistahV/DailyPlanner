042c251a7d16b772295b6cbf4af7783861260295
chrisvanetten <christophervanetten@gmail.com>
2021-08-11 12:59:47 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    //display current date on the page - moment, getdocumentbyID
+    
+    //user clicks save button..
+    
+       function saveTask()
+        //get stuff user put in textarea
+        //add that value to localstorage
+        //show message
+        //hide message after so many seconds
+    
+    
+       $('.saveBtn').on('click', saveTask);
+    
+       function updateRowColor() {
+        //set variable to current time
+        //loop over each row of the class (time-block)
+          //find row time using data attribute (9 < 21)
+          //add 'past' css class by doing $(this).addCLass('past');
+            //use if else...
+     
+    
+     //color rows based on the time of day --> get right class from css
+    
+       updateRowColor();
+    
+       } 
+    
+     //load data from local storage and put it in the correct row
+     //set hourID textare to value of local storage key number 'hourID'
+    
+     
+     //display current date on the age - moment (getdocumentbyID)
+    })
+    
